3220f6a11fe29f99be51bbf63c19e7cb75ab9120
RuslanDabysov <ruslandabysov@gmail.com>
2022-11-06 14:05:13 +0300

    prepare page for react

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников в компании Крокодил</h1>
+            <h2>Общее число сотрудников: </h2>
+            <h3>Премию получат: </h3>
+        </div>
+    )
+}
+
+export default AppInfo;

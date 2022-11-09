37935d1ca014425ca2139dd7746539a2901b2b13
RuslanDabysov <ruslandabysov@gmail.com>
2022-11-09 22:17:29 +0300

    adding new features

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -19,11 +19,26 @@ class App extends Component {
                 {name: 'Carl L.', salary: 1120, increase: true, id: 3}
             ]
         }
+        this.maxId = 4;
     }
     deleteItem = (id) => {
         this.setState(({data}) => {
             return {
                 data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem]
+            return {
+                data: newArr
             }
         })
     }
@@ -39,7 +54,7 @@ class App extends Component {
                 </div>
                 <EmployeesList data={this.state.data}
                 onDelete={this.deleteItem} />
-                <EmployeesAddForm/>
+                <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         );
     }
